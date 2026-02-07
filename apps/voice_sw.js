0d697d3e9c5ca05096102c1ad29e8d541997c102
maicel1978 <maicel1978@users.noreply.github.com>
2026-02-07 19:18:13 +0000

    deploy: 9dc62c8c546fb142531de5952442c0359d401036

---
+++ apps/voice_sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'bioedu-voz-v1';
+const urlsToCache = [
+  'audio_voice_mimic.html',
+  'voice_mimic.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
